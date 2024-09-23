5e9a84f07ca0901f8264c539cdbfddcbf236b146
zetasuna <nguyenducnam2107@gmail.com>
2024-09-24 01:09:33 +0700

    add docker

--- app/libs/db.tsx
+++ app/libs/db.tsx
@@ -4,6 +4,7 @@ export const createConnection = async () => {
   
   const connection = await mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_SCHEMA,
